8068d645ec99336c838624452a230b04c803593c
sathwikhbhat <sathwikhbhat@gmail.com>
2025-02-10 16:31:04 +0530

    Used Lombok instead of Getters and Setters

--- src/main/java/com/sathwikhbhat/journalApp/entity/JournalEntry.java
+++ src/main/java/com/sathwikhbhat/journalApp/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.sathwikhbhat.journalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,44 +12,11 @@ import java.util.Date;
 import java.util.Objects;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
-
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
 }
