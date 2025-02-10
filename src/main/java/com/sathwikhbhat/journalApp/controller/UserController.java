578d9e5b4fe63bee4a82859080bf1e67eeb67e29
sathwikhbhat <sathwikhbhat@gmail.com>
2025-02-10 17:04:51 +0530

    Added Log-in Functionality

---
+++ src/main/java/com/sathwikhbhat/journalApp/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.sathwikhbhat.journalApp.controller;
+
+import com.sathwikhbhat.journalApp.entity.User;
+import com.sathwikhbhat.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user) {
+        userService.saveEntry(user);
+    }
+
+    @PutMapping("/{username}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String username) {
+        User userInDB = userService.findByUsername(username);
+        if (userInDB != null) {
+            userInDB.setUsername(user.getUsername());
+            userInDB.setPassword(user.getPassword());
+            userService.saveEntry(userInDB);
+        }
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
