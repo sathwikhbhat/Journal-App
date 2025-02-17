feb0418465623130ee3c3c5917e2b6f5c2845648
sathwikhbhat <sathwikhbhat@gmail.com>
2025-02-17 07:23:58 +0530

    Formatting Code

--- src/main/java/com/sathwikhbhat/journalApp/entity/JournalEntry.java
+++ src/main/java/com/sathwikhbhat/journalApp/entity/JournalEntry.java
@@ -8,8 +8,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Objects;
 
 @Document(collection = "journal_entries")
 @Data
