155fc0ed7410534f21699a1493c80ad96411f457
sathwikhbhat <sathwikhbhat@gmail.com>
2025-05-28 12:05:22 +0530

    Added @NonNull annotation to title in JournalEntry

--- src/main/java/com/sathwikhbhat/journalApp/entity/JournalEntry.java
+++ src/main/java/com/sathwikhbhat/journalApp/entity/JournalEntry.java
@@ -1,6 +1,7 @@
 package com.sathwikhbhat.journalApp.entity;
 
 import lombok.Data;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 public class JournalEntry {
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
