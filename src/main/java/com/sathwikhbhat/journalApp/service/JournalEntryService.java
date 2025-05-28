bf73024491e3f18bc903ff390d85216e0dca9678
sathwikhbhat <sathwikhbhat@gmail.com>
2025-05-28 14:49:50 +0530

    Enabled transactional support for journal entry operations

--- src/main/java/com/sathwikhbhat/journalApp/service/JournalEntryService.java
+++ src/main/java/com/sathwikhbhat/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,6 +22,7 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
         try {
             User user = userService.findByUserName(userName);
