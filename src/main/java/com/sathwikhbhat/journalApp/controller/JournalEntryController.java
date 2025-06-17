ee0b7425ba0fd39d05b21c77bd8f0bff8701cd2f
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-06-17 09:42:10 +0530

    Removed an unused import statement

--- src/main/java/com/sathwikhbhat/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/sathwikhbhat/journalApp/controller/JournalEntryController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
